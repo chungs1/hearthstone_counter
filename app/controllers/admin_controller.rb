12003a2ae68ec7879a1bd1de9a79bcdcfb7489b0
Kevin Casey <kcasey@godaddy.com>
2014-07-22 19:47:00 -0700

    forgot session

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,7 @@ class AdminController < ApplicationController
 
   def login_post
     if ENV['secret_password'] == params[:password]
+      session['authenticated'] = true
       return redirect_to root_path, notice: "Nice, you have admin privileges"
     end
     redirect_to login_path, alert: "Oops wrong password"
